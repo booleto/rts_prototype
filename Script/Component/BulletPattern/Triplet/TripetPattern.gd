2052b22775aff874bd361feb7939df3a02710d14
booleto <exaccxd@gmail.com>
2023-12-31 23:58:17 +0700

    actually finalise the system

--- Script/Component/BulletPattern/Triplet/TripetPattern.gd
+++ Script/Component/BulletPattern/Triplet/TripetPattern.gd
@@ -12,12 +12,11 @@ func _physics_process(delta):
 	super(delta)
 	if not casting:
 		return
-	if physics_tick / 2 <= 1:
+	for i in range(3):
 		bullet_temp = bullet_prefab.instantiate()
 		bullet_temp.movement_pattern = pattern
-		bullet_temp.position += Vector2.from_angle(shot_angle + (physics_tick-1) * 90) * 100
+		bullet_temp.position += Vector2.from_angle(shot_angle + (i-1) * 90) * 40
 		bullet_temp.rotation = shot_angle
 		bullet_temp.bullet_lifetime = 10
 		add_child(bullet_temp)
-	else:
-		stop()
+	stop()
